11237bf204fe88a33c29decda469068787cf4485
marcelo-souto <marcelosouto676@gmail.com>
2023-12-30 19:10:09 -0300

    -

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDTO } from './dto/sign-in-dto';
 import { ValidationPipe } from '@nestjs/common';
@@ -8,6 +8,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UsePipes(new ValidationPipe())
+  @HttpCode(200)
   @Post('login')
   signIn(@Body() signInDTO: SignInDTO) {
     const { email, password } = signInDTO;
